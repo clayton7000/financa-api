ca8f7f8ded7754a0c8e801585d84a032ac70b5a4
clayton7000 <80071108+clayton7000@users.noreply.github.com>
2022-11-02 10:23:29 -0300

    atualizei o projeto colocando o swagger

--- src/main/java/com/financa/demo/controller/EntradaSaidaController.java
+++ src/main/java/com/financa/demo/controller/EntradaSaidaController.java
@@ -8,17 +8,21 @@ package com.financa.demo.controller;
 import com.financa.demo.model.EntradaSaida;
 import com.financa.demo.repository.EntradaSaidaRepository;
 import com.financa.demo.service.EntradaSaidaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,12 +31,16 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author clayt
  */
+@CrossOrigin(origins = "*")
 @RestController
+@RequestMapping(path = "/entradaSaida")
+@Api(value="API REST entrada e saida de dinheiro das finanças")
 public class EntradaSaidaController {
 
     @Autowired
     private EntradaSaidaService service;
 
+    @ApiOperation(value="Retorna uma lista de Produtos")
     @GetMapping
     public List<EntradaSaida> getAll() {
         return service.getAll();
@@ -45,6 +53,7 @@ public class EntradaSaidaController {
 
     }
     
+    @ApiOperation(value="Retorna uma lista de Produtos")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public EntradaSaida insert(@RequestBody EntradaSaida entradaSaida) {
